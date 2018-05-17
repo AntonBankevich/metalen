1b6b976d868acbfdb339998c6aef6530db3b59fa
Anton <anton.bankevich@gmail.com>
2018-05-17 23:20:07 +0300

    tools modified

--- tools/sam_to_stats.py
+++ tools/sam_to_stats.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.append("py")
 from meta_length import sam_parser, SeqIO, metalen_io
 
 class Record:
@@ -10,14 +11,19 @@ class Record:
 def CollectRecords(fasta_file):
     res = dict()
     for rec in SeqIO.parse_fasta(metalen_io.universal_read(fasta_file)):
-        res[rec.id] = Record(rec.id), len(rec)
+        res[rec.id] = Record(rec.id, len(rec))
     return res
 
 if __name__ == "__main__":
     fasta_file = sys.argv[1]
     recs = CollectRecords(fasta_file)
     sam_file = sys.argv[2]
+    cnt = 1
     for rec in sam_parser.Samfile(open(sam_file, "r")):
+        cnt += 1
+        if cnt % 1000000 == 0:
+            sys.stderr.write(str(cnt) + "\n")
+            sys.stderr.flush()
         if not rec.is_unmapped:
             recs[rec.tname].count += 1
     recs = recs.values()
